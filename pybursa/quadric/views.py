45665c77493895c670e83f056b72239ca79b590f
Chan <evgeniychan@gmail.com>
2017-11-20 13:16:15 +0300

    end of lesson3 part 2  week4

--- pybursa/quadric/views.py
+++ pybursa/quadric/views.py
@@ -9,31 +9,57 @@ def index(request):
     return response
 
 
+from django import forms
+
+
+class QuadricForms(forms.Form):
+    a = forms.CharField(max_length=100)
+    b = forms.CharField(max_length=100)
+    c = forms.CharField(max_length=100)
+
+
 def results(request):
-    # resp = "a = {} b = {} c = {}".format(a, b, c)
+    form = QuadricForms()
+    print(request.GET.get('a'))
     a = request.GET.get('a')
     b = request.GET.get('b')
     c = request.GET.get('c')
-    aa = False
-    try:
-        a = int(a)
-        b = int(b)
-        c = int(c)
+    resp = "a = {} b = {} c = {}".format(a, b, c)
+    # resp['form'] = form
+    def tryse(z):
+        try:
+            z = int(z)
+            return z
+        except:
+            z = 'error'
+            return z
 
-    except:
-        aa = True
+    a = tryse(a)
+    b = tryse(b)
+    c = tryse(c)
+    errors = {
+        'a_error': False,
+        'b_error': False,
+        'c_error': False
+    }
+    if a == 'error':
+        errors['a_error'] = 'Error'
+    if b == 'error':
+        errors['b_error'] = 'Error'
+    if c == 'error':
+        errors['c_error'] = 'Error'
 
-    if aa == True:
-        resp = "a = {} b = {} c = {}, koefficient ne opredelen".format(a, b, c)
-    elif int(a) == 0:
-        resp = "a = {} b = {} c = {}, a nt mozhet bit ravno 0".format(a, b, c)
-        print(2)
+    if a != 'error' and b != 'error' and c != 'error' and a != 0:
+        resp = int(a) ** 2 + int(b) + int(c)
+    elif a == 0:
+        resp = 'a - Ne mozet bit nulem'
     else:
-        resp = int(a) + int(b) + int(c)
-        print(4)
-    # response = HttpResponse(resp)
-    # return response
+        resp = 'Oshibka Argumetna'
     return render(request, 'result.html', {'resp': resp,
                                            'a': a,
                                            'b': b,
-                                           "c": c}, )
+                                           'c': c,
+                                           'a_error': errors['a_error'],
+                                           'b_error': errors['b_error'],
+                                           'c_error': errors['c_error'],
+                                           'form': form}, )
